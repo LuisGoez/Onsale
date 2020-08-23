1b67b315553deafcfa332b239e0f7a2b46fcda73
lgoez <lgoez@intergrupo.com>
2020-08-23 11:44:11 -0500

    add api with security

--- OnSale.Web/Controllers/API/AccountController.cs
+++ OnSale.Web/Controllers/API/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using OnSale.Common.Requests;
 using OnSale.Web.Data.Entities;
 using OnSale.Web.Helpers;
 using OnSale.Web.Models;
@@ -23,6 +26,25 @@ namespace OnSale.Web.Controllers.API
         {
             _userHelper = userHelper;
             _configuration = configuration;
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        [Route("GetUserByEmail")]
+        public async Task<IActionResult> GetUserByEmail([FromBody] EmailRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            User user = await _userHelper.GetUserAsync(request.Email);
+            if (user == null)
+            {
+                return NotFound("Error001");
+            }
+
+            return Ok(user);
         }
 
         [HttpPost]
